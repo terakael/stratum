637e22cb12d3ff78fb35727812a2ca1bc93fb8fb
Knezevic, Daniel | Dan | ECFD <daniel.knezevic@rakuten.com>
2026-03-23 20:18:37 +0900

    [dynamic] extended channel miner tag

--- sv2/channels-sv2/src/server/extended.rs
+++ sv2/channels-sv2/src/server/extended.rs
@@ -280,6 +280,27 @@ where
         self.chain_tip = Some(chain_tip);
     }
 
+    /// Updates the miner tag for future jobs.
+    ///
+    /// This allows dynamic modification of the miner tag without recreating the channel.
+    /// Validates that the new tag won't exceed scriptSig size limits.
+    pub fn set_miner_tag(&mut self, miner_tag: Option<String>) -> Result<(), ExtendedChannelError> {
+        let script_sig_size = 5 + // BIP34
+            1 + // OP_PUSHBYTES
+            3 + // `/` delimiters
+            self.job_factory.pool_tag_string.as_ref().map_or(0, |s| s.len()) +
+            miner_tag.as_ref().map_or(0, |s| s.len()) +
+            1 + // OP_PUSHBYTES for extranonce
+            self.get_full_extranonce_size();
+
+        if script_sig_size > 100 {
+            return Err(ExtendedChannelError::ScriptSigSizeTooLarge);
+        }
+
+        self.job_factory.set_miner_tag(miner_tag);
+        Ok(())
+    }
+
     /// Updates the extranonce prefix for this channel.
     ///
     /// After this call, all newly created jobs will reference the new prefix.
